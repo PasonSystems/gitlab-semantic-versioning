7ad99f3d5acef21f143a809a2be8251ec2191ee3
Justin Chu <jchu@pason.com>
2025-08-25 10:49:04 -0600

    STELLAR-3514: updating command to pull origin masters tag

--- version-update.py
+++ version-update.py
@@ -74,7 +74,7 @@ def main():
     [verify_env_var_presence(e) for e in env_list]
 
     try:
-        latest = git("describe", "--tags", "--first-parent", "--match", "[[:digit:]]*.[[:digit:]]*.[[:digit:]]*").decode().strip()
+        latest = git("describe", "--tags", "--first-parent", "origin/master", "--match", "[[:digit:]]*.[[:digit:]]*.[[:digit:]]*").decode().strip()
     except subprocess.CalledProcessError:
         # Default to version 1.0.0 if no tags are available
         version = "1.0.0"
